35a37aae1ad0e7fa6251a59cede143541ae2f07d
William Alves da Silva <32403341+WillcraftGamepy@users.noreply.github.com>
2022-08-23 20:06:51 -0300

    Add files via upload

---
+++ testegame.py
@@ -0,0 +1,18 @@
+print("Bem vindo, jogador, ao alpha")
+digite=str(input("Jogar? "))
+if digite=="Sim" or "sim" or "s":
+    contador = 0
+    soma_contador = 0.001
+    while contador < 100:
+        contador += soma_contador
+        print('Loading', int(contador), '%')
+elif digite=="Não" or "não" or "n":
+     quit()
+print("Esse mundo era místico, magia por todos os lados, um lugar de alegria e paz, havia 5 10 grandes nações antes estavam sempre em guerra por território")
+print("mas não havia desde da 2º Guerra Mística, houve várias mortes e feridos, que por pouco não acordou o monstro do mundo, que estava descansando no vulcão, se acordasse")
+print("a destruição seria gigante em todo o mundo, milhões de pessoas, animais e seres mitológicas poderiam morrer e o mundo desabitado")
+print("mais um grande mal está prestes a ameaçar paz e a harmonia, um mostro com fome, devorador de planetas, todos os reinos se reuniram para detê-lo, mas não foi possível")
+print("Uma profecia bem antiga, diz que uma pessoa poderá controlar todas as classes, dominar todos os poderes ancestrais, derrotar o vilão que ameaça a vida e salvar o mundo")
+print("e essa pessoa É VOCÊ!!!")
+print("Você acorda depois de um longo sono de almoço e viu o caos em sua volta e as pessoas dizendo")
+print("- É o fim, vamos apressar, vamos correr pra longe")
