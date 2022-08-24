d1358176a183b44385108951e7dfbcc1066f8189
William Alves da Silva <32403341+WillcraftGamepy@users.noreply.github.com>
2022-08-23 21:30:16 -0300

    Add files via upload
    EN
    Updated game file, feel free to edit
    
    PT-BR
    Arquivo do jogo Atualizado, sintam-se livres pra editar

--- testegame.py
+++ testegame.py
@@ -14,5 +14,23 @@ print("a destruição seria gigante em todo o mundo, milhões de pessoas, animai
 print("mais um grande mal está prestes a ameaçar paz e a harmonia, um mostro com fome, devorador de planetas, todos os reinos se reuniram para detê-lo, mas não foi possível")
 print("Uma profecia bem antiga, diz que uma pessoa poderá controlar todas as classes, dominar todos os poderes ancestrais, derrotar o vilão que ameaça a vida e salvar o mundo")
 print("e essa pessoa É VOCÊ!!!")
-print("Você acorda depois de um longo sono de almoço e viu o caos em sua volta e as pessoas dizendo")
+print("acordei depois de um longo sono de almoço e viu o caos em sua volta e as pessoas dizendo")
 print("- É o fim, vamos apressar, vamos correr pra longe")
+print("Você vai pro foco e vê a causa do reboliço que está rolando, perguntei pro mago o que o que tá acontecendo e fez uma magia no meu olho pra que possa ver me muito longe por 1 minuto")
+print("que é um monstro espacial gigante, uma boca gigantesca pra frente com dentes afiados, com mais de 100.000 metros de altura, devorando mundos, estrelas e luas, sempre com fome")
+print("há 130 trilhoes de quilometros do planeta, e pensei: ih mané, já meteu essa? é sério que eu vou morrer solteiro e sem me casar com uma linda maga mesmo?")
+print("- Você é o herói da profecia, se dominar todos os poderers, poderá salvar milhões de vidas- Comentou o mago")
+print("Bora lá- respondo sem pensar")
+print("na nossa jornada, encontrei uma maga linda, a maga dos meus sonhos, cabelo longo, olhos puxados e um rosto belo, tipo aquelas de princesas")
+print("perguntei qual era o nome dela -O meu nome é Zaiff, sou recém-formada na escola pra magos em The Powerful Lakes of Oeaa- repondeu")
+print("é uma das escolas de magia mais famosas do mundo, que louco, estudei lá tbm, não lembro de você lá, eu era da turma de manhã")
+print("-Que legal, era da turma de tarde então, prazer em te conhecer- falou")
+digite-str(input("Hmmmmm, então, convido ela ou não?, ela é gente boa, mas como toda a história de profecia, etc e tals, pode ser uma vilã"))
+class switch(digite):
+    if case(1):
+        print("- Venha comigo, vem me achompanhar eu e o mago na jornada de salvar o mundo, mas não me traíkkk, pq todo mundo está em perigo")
+        print(" - KKKKKKKKKKKKKKKKKK, tranquilo, n vou te trair e posso te ajudar")
+        break
+    if case(2):
+        print("- Vou indo tá, me passa o seu contato, quando as coisas se alcamar, podemos sair, que tal?")
+        print("- Beleza, vou passar rapidinho - passou o contato - e boa sorte, to te esperando - me beijou na bochecha e sorriu pra mim")
