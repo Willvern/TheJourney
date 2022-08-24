96086e575147ed509a3738964855daf321d62e4e
William Alves da Silva <32403341+WillcraftGamepy@users.noreply.github.com>
2022-08-24 17:13:27 -0300

    Add files via upload

--- testegame.py
+++ testegame.py
@@ -1,4 +1,4 @@
-print("Bem vindo, jogador, ao alpha")
+print("Bem vindo, jogador, ao alpha do The Jorney")
 digite=str(input("Jogar? "))
 if digite=="Sim" or "sim" or "s":
     contador = 0
@@ -8,29 +8,44 @@ if digite=="Sim" or "sim" or "s":
         print('Loading', int(contador), '%')
 elif digite=="Não" or "não" or "n":
      quit()
-print("Esse mundo era místico, magia por todos os lados, um lugar de alegria e paz, havia 5 10 grandes nações antes estavam sempre em guerra por território")
-print("mas não havia desde da 2º Guerra Mística, houve várias mortes e feridos, que por pouco não acordou o monstro do mundo, que estava descansando no vulcão, se acordasse")
-print("a destruição seria gigante em todo o mundo, milhões de pessoas, animais e seres mitológicas poderiam morrer e o mundo desabitado")
-print("mais um grande mal está prestes a ameaçar paz e a harmonia, um mostro com fome, devorador de planetas, todos os reinos se reuniram para detê-lo, mas não foi possível")
-print("Uma profecia bem antiga, diz que uma pessoa poderá controlar todas as classes, dominar todos os poderes ancestrais, derrotar o vilão que ameaça a vida e salvar o mundo")
-print("e essa pessoa É VOCÊ!!!")
+print("Esse mundo era místico, magia por todos os lados, um lugar de alegria e paz, havia 10 grandes nações antes estavam sempre em guerra por território\n mas não havia desde da 2º Guerra Mística, houve várias mortes e feridos, que por pouco não acordou o monstro do mundo, que estava descansando no vulcão, se acordasse\n a destruição seria gigante em todo o mundo, milhões de pessoas, animais e seres mitológicas poderiam morrer e o mundo desabitado\n mais um grande mal está prestes a ameaçar paz e a harmonia, um mostro com fome, devorador de planetas, todos os reinos se reuniram para detê-lo, mas não foi possível\n Uma profecia bem antiga, diz que uma pessoa poderá controlar todas as classes, dominar todos os poderes ancestrais, derrotar o vilão que ameaça a vida e salvar o mundo \ne essa pessoa É VOCÊ!!!")
 print("acordei depois de um longo sono de almoço e viu o caos em sua volta e as pessoas dizendo")
 print("- É o fim, vamos apressar, vamos correr pra longe")
-print("Você vai pro foco e vê a causa do reboliço que está rolando, perguntei pro mago o que o que tá acontecendo e fez uma magia no meu olho pra que possa ver me muito longe por 1 minuto")
-print("que é um monstro espacial gigante, uma boca gigantesca pra frente com dentes afiados, com mais de 100.000 metros de altura, devorando mundos, estrelas e luas, sempre com fome")
-print("há 130 trilhoes de quilometros do planeta, e pensei: ih mané, já meteu essa? é sério que eu vou morrer solteiro e sem me casar com uma linda maga mesmo?")
-print("- Você é o herói da profecia, se dominar todos os poderers, poderá salvar milhões de vidas- Comentou o mago")
-print("Bora lá- respondo sem pensar")
-print("na nossa jornada, encontrei uma maga linda, a maga dos meus sonhos, cabelo longo, olhos puxados e um rosto belo, tipo aquelas de princesas")
-print("perguntei qual era o nome dela -O meu nome é Zaiff, sou recém-formada na escola pra magos em The Powerful Lakes of Oeaa- repondeu")
-print("é uma das escolas de magia mais famosas do mundo, que louco, estudei lá tbm, não lembro de você lá, eu era da turma de manhã")
-print("-Que legal, era da turma de tarde então, prazer em te conhecer- falou")
-digite-str(input("Hmmmmm, então, convido ela ou não?, ela é gente boa, mas como toda a história de profecia, etc e tals, pode ser uma vilã"))
-class switch(digite):
-    if case(1):
-        print("- Venha comigo, vem me achompanhar eu e o mago na jornada de salvar o mundo, mas não me traíkkk, pq todo mundo está em perigo")
-        print(" - KKKKKKKKKKKKKKKKKK, tranquilo, n vou te trair e posso te ajudar")
-        break
-    if case(2):
-        print("- Vou indo tá, me passa o seu contato, quando as coisas se alcamar, podemos sair, que tal?")
-        print("- Beleza, vou passar rapidinho - passou o contato - e boa sorte, to te esperando - me beijou na bochecha e sorriu pra mim")
+print("Você vai pro foco e vê a causa do reboliço que está rolando, perguntei pro mago o que o que tá acontecendo e fez uma magia no meu olho pra que possa ver me muito longe por 1 minuto \nque é um monstro espacial gigante, uma boca gigantesca pra frente com dentes afiados, com mais de 100.000 metros de altura, devorando mundos, estrelas e luas, sempre com fome\n há 130 trilhoes de quilometros do planeta, e pensei: ih mané, já meteu essa? é sério que eu vou morrer solteiro e sem me casar com uma linda maga mesmo?\n- Você é o herói da profecia, se dominar todos os poderers, poderá salvar milhões de vidas- Comentou o mago")
+print("respondo sem pensar- Bora lá - começamos a jornada.")
+print("durante a nossa jornada, encontrei uma maga linda, a maga dos meus sonhos, cabelo longo, olhos puxados e um rosto belo, tipo aquelas de princesas. \nPerguntei qual era o nome dela")
+print("Desconhecida- O meu nome é Zaiff, sou recém-formada na escola pra magos em Oeaa- repondeu")
+print("Eu - é uma das escolas de magia mais famosas do mundo, que louco, estudei lá tbm, não lembro de você lá, eu era da turma de manhã")
+print("Zaiff - Que legal, era da turma de tarde então, prazer em te conhecer")
+escolha=str(input("Pensando: Hmmmmm, então, convido ela ou não?, ela é gente boa, mas como toda a história de profecia, etc e tals, pode ser uma vilã"))
+if escolha=="Sim" or "sim" or "s":
+    print("Eu: Você gostarria de ir comigo nesse jornada de salvar o mundo?")
+    print("Zaiff: O que pode dar de errado KKKKKKK, então, bora lá!, e não vou te trair, eu juro, se eu ameaçar, pode me matar")
+    print("E Zaiff se juntou a guilda")
+elif escolha=="Não" or "não" or "n":
+    print("-Estou ocupado tentando salvar o mundo, mas passa seu contato pra gente conversar, se a gente sobreviver, te chamo pra um encontro, se caso rolar algum tcham")
+    print("Zaiff -Beleza, aqui o meu número, se quiser chamar, liga aí")
+    print("Ela passou o contato e me deu um beijo, pensei- KKKKK se caso a gente morrer, a gente se beijou")
+lugar=str(input("Pra onde vamos?"))
+def lugar:
+    match lugar:
+        case 1:
+            print("Bora para Oeaa, além de bonito, mora o pessoal de elite, os caras são brabos d+")
+        case 2:
+            print("Bora para Aliurya")
+        case 3:
+            print("Bora para")
+        case 4:
+            print("Bora pra ")
+        case 5:
+            print("Que tal ")
+        case 6:
+            print("Acho melhor se for para , tem magias fortíssimas lá")
+        case 7:
+            print("Bora para , Os guerreiros e magos são experientes, podem te monstrar tecnicas")
+        case 8:
+            print("Tava pensand em  lá tem um pessoal forte")
+        case 9:
+            print("Bora lá pra ...")
+        case 10:
+            print("Bora pra ... então?")
