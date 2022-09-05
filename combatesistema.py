24f8add616e0caf493d60528cb97e1915ab42dcc
William Alves da Silva <32403341+WillcraftGamepy@users.noreply.github.com>
2022-09-05 17:15:39 -0300

    Add files via upload

--- combatesistema.py
+++ combatesistema.py
@@ -1,15 +1,3 @@
-def acao():
-    match acao:
-        case 1:
-            ("")
-
-
-def inventario():
-    match inventario:
-        case porcao:
-            print("O",personagem,"tomou a porção de cura!")
-
-
 def combate_inicio(enemy):
     print("Você encontra um",enemy,"e todos se juntaram a você")
     escolha= str(input("O que você e sua guilda vão reagir "))
@@ -29,6 +17,7 @@ def lutar():
             atk=str(input("Escolheu os poderes de mago\n1º - Bola de Fogo\nº2 - Furação do Dragão\n 3º - Portal \n 4º - Vortex de água\nescolha o poder entre 1 e 4: "))
             if atk==1:
                 print("Bola de Fogo!")
+                
             elif atk==2:
                 print("Furação do Dragão!")
             elif atk==3:
@@ -36,4 +25,13 @@ def lutar():
             elif atk==4:
                 print("Vortex de água!")
             
+def acao():
+    match acao:
+        case 1:
+            ("")
 
+
+def inventario():
+    match inventario:
+        case porcao:
+            print("O",personagem,"tomou a porção de cura!")
