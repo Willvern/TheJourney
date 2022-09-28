96fbe0e3ef99cb3bdf589398b6079090bf05a911
William Alves da Silva <32403341+WillcraftGamepy@users.noreply.github.com>
2022-09-28 13:39:05 -0300

    Add files via upload

---
+++ thejourney.py
@@ -0,0 +1,53 @@
+#import combatesistema
+import time
+print("Bem vindo, jogador, ao alpha do The Journey")
+digite=str(input("Jogar? "))
+if digite=="Sim" or "sim" or "s":
+    contador = 0
+    soma_contador = 0.001
+    for contador in range(0,101):
+        #contador += soma_contador
+        print('Loading', int(contador), '%')
+        time.sleep(0.7)
+elif digite=="Não" or "não" or "n":
+    ("")
+print("Esse mundo era místico, magia por todos os lados, um lugar de alegria e paz, havia 10 grandes nações antes estavam sempre em guerra por território \n mas não havia desde da 2º Guerra Mística, houve várias mortes e feridos, que por pouco não acordou o monstro do mundo, que estava descansando no vulcão, se acordasse\n a destruição seria gigante em todo o mundo, milhões de pessoas, animais e seres mitológicas poderiam morrer e o mundo desabitado\n mais um grande mal está prestes a ameaçar paz e a harmonia, um mostro com fome, devorador de planetas, todos os reinos se reuniram para detê-lo, mas não foi possível\n Uma profecia bem antiga, diz que uma pessoa poderá controlar todas as classes, dominar todos os poderes ancestrais, derrotar o vilão que ameaça a vida e salvar o mundo \ne essa pessoa É VOCÊ!!!")
+print("Acordei depois de um longo sono de almoço e viu o caos em sua volta e as pessoas dizendo")
+print("- É o fim, vamos apressar, vamos correr pra longe")
+print("Você vai pro foco e vê a causa do reboliço que está rolando, perguntei pro mago o que o que tá acontecendo e fez uma magia no meu olho pra que possa ver me muito longe por 1 minuto \nque é um monstro espacial gigante, uma boca gigantesca pra frente com dentes afiados, com mais de 100.000 metros de altura, devorando mundos, estrelas e luas, sempre com fome\n há 130 trilhoes de quilometros do planeta, e pensei: ih mané, já meteu essa? é sério que eu vou morrer solteiro e sem me casar com uma linda maga mesmo?\n- Você é o herói da profecia, se dominar todos os poderers, poderá salvar milhões de vidas- Comentou o mago\n\nrespondo sem pensar- Bora lá - começamos a jornada.")
+print("durante a nossa jornada, encontrei uma maga linda, a maga dos meus sonhos, cabelo longo, olhos puxados e um rosto belo, tipo aquelas de princesas. \nEu: Quem é você?")
+print("Desconhecida- O meu nome é Zaiff, sou recém-formada na escola pra magos em Oeaa, estava em missão, mas agora tá um caos por causa do alien, que desistiu")
+print("Eu - é uma das escolas de magia mais famosas do mundo, que louco, estudei lá tbm, não lembro de você lá, eu era da turma de manhã")
+print("Zaiff - Que legal, era da turma de tarde então, prazer em te conhecer")
+escolha=str(input("Pensando: Hmmmmm, então, convido ela ou não?, ela é gente boa, mas como toda a história de profecia, etc e tals, pode ser uma vilã "))
+if escolha=="Sim" or "sim" or "s":
+    print("Eu: Você gostarria de ir comigo nesse jornada de salvar o mundo?")
+    print("Zaiff: O que pode dar de errado KKKKKKK, então, bora lá!, e não vou te trair, eu juro, se eu ameaçar, pode me matar")
+    print("E Zaiff se juntou a guilda")
+elif escolha=="Não" or "não" or "n":
+    print("-Estou ocupado tentando salvar o mundo, mas passa seu contato pra gente conversar, se a gente sobreviver, te chamo pra um encontro, se caso rolar algum tcham")
+    print("Zaiff -Beleza, aqui o meu número, se quiser chamar, liga aí")
+    print("Ela passou o contato e me deu um beijo, pensei- KKKKK se caso a gente morrer, a gente se beijou")
+lugar=str(input("Pra onde vamos? "))
+def lugar():
+    match lugar:
+        case 1:
+            print("Bora para Oeaa, além de bonito, mora o pessoal de elite, os caras são brabos d+")
+        case 2:
+            print("Bora para Aliurya")
+        case 3:
+            print("Bora para")
+        case 4:
+            print("Bora pra ")
+        case 5:
+            print("Que tal ")
+        case 6:
+            print("Acho melhor se for para , tem magias fortíssimas lá")
+        case 7:
+            print("Bora para , Os guerreiros e magos são experientes, podem te monstrar tecnicas")
+        case 8:
+            print("Tava pensand em  lá tem um pessoal forte")
+        case 9:
+            print("Bora lá pra ...")
+        case 10:
+            print("Bora pra ... então?")
